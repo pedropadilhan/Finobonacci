86fb804835d2040a2cdbddcb4e5fc4c8185f9da8
Pedro Padilha Neto <pedropadilhan@gmail.com>
2023-03-07 20:43:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Finobonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("FIBONACCI");
+            int n1 = 0;
+            int n2 = 1;
+            int soma = 0;
+
+            for(int i = 1; i < 20; i++)
+            {
+                soma = n1 + n2;
+                n2 = n1;
+                n1 = soma;
+
+                Console.Write(soma+",");
+            }
+        }
+    }
+}
